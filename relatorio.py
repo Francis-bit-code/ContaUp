3031641231d3ffb03142b64d3a82c07f8caebc92
José Eduardo Idelfonsio <joseidelfonsio123@gmail.com>
2026-04-07 21:17:37 -0300

    Update relatorio.py

---
+++ relatorio.py
@@ -0,0 +1,17 @@
+#puxar banco de dados
+
+while True:
+    print('=-=-=-=-=-=-=-= Menu =-=-=-=-=-=-=-=')
+    print('1 - Ver despesas\n2 - Ver lucros\n3 - Voltar')
+    user_escolha = int(input())
+    if user_escolha == 1:
+        #codigo puxando do banco de dados
+        print('Despesas')
+    elif user_escolha == 2:
+        #codigo puxando do banco de dados
+        print('Despesas')
+    elif user_escolha == 3:
+        #voltar para menu principal
+        break
+    else:
+        print('Opção inválida, favor selecione novamente.')
